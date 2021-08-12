93114d75e1c625287823754313c8e8456de98689
DanielRGilles <danryangilles@gmail.com>
2021-08-12 15:31:57 -0700

    building calculator

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // import functions and grab DOM elements
-
+import {add, subtract, divide, multiply} from './utils.js'
 // initialize state
+const numOne = document.getElementById('num-one')
+const numTwo = document.getElementById('num-two')
+const resultEl = document.getElementById('math-result')
+
+
+
 
 // set event listeners 
   // get user input
