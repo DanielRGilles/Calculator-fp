fa63c1418cb87642f92fcfc6d38a1ef477ae83cb
DanielRGilles <danryangilles@gmail.com>
2021-08-12 16:08:31 -0700

    functional calculation

--- app.js
+++ app.js
@@ -1,14 +1,55 @@
 // import functions and grab DOM elements
-import {add, subtract, divide, multiply} from './utils.js'
+import {
+  add,
+  subtract,
+  divide,
+  multiply
+} from './utils.js'
 // initialize state
 const numOne = document.getElementById('num-one')
 const numTwo = document.getElementById('num-two')
 const resultEl = document.getElementById('math-result')
-
+const addBtn = document.getElementById('add-num')
+const subBtn = document.getElementById('sub-num')
+const divBtn = document.getElementById('div-num')
+const multBtn = document.getElementById('mult-num')
 
 
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+addBtn.addEventListener('click', () => {
+  const firstValue = numOne.value;
+  const secondValue = numTwo.value;
+  
+  const sum = add(firstValue, secondValue);
+
+  resultEl.textContent = sum;
+});
+subBtn.addEventListener('click', () => {
+  const firstValue = numOne.value;
+  const secondValue = numTwo.value;
+
+  const sum = subtract(firstValue, secondValue);
+
+  resultEl.textContent = sum;
+});
+divBtn.addEventListener('click', () => {
+  const firstValue = numOne.value;
+  const secondValue = numTwo.value;
+
+  const sum = divide(firstValue, secondValue);
+
+  resultEl.textContent = sum;
+});
+multBtn.addEventListener('click', () => {
+  const firstValue = numOne.value;
+  const secondValue = numTwo.value;
+
+  const sum = multiply(firstValue, secondValue);
+
+  resultEl.textContent = sum;
+});
+
+// get user input
+// use user input to update state 
+// update DOM to reflect the new state
