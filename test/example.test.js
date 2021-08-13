4270a14cc8ceb4c45b16e9104405c4a9316d7670
DanielRGilles <danryangilles@gmail.com>
2021-08-12 17:45:40 -0700

    worked on testing, cleaned up code

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,23 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import {
+    add,
+    subtract,
+    divide,
+    multiply
+  } from '../utils.js'
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
+// test('time to test a function', add(4,5) => {
+//     //Arrange
+//     // Set up your arguments and expectations
+//     const expectedNum = '9' ;
     
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const actualNum = '';
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
-});
+//     //Expect
+//     // Make assertions about what is expected versus the actual result
+//     expectedNum.add(actual, expected);
+// });
